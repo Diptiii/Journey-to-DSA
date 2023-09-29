0ffc7c6fc03b5eb8f175ef6ef11125214a99a366
Diptiii <90438170+Diptiii@users.noreply.github.com>
2023-09-29 09:54:40 +0530

    Update reverse a string.java

--- BASIC DSA/Strings/reverse a string.java
+++ BASIC DSA/Strings/reverse a string.java
@@ -1,1 +1,23 @@
-S
+import java.util.*;
+
+public class Questions {
+    public static void main(String args[]) {
+
+                StringBuilder greeting=new StringBuilder("Hello");
+                int n=greeting.length();
+                for(int i=0;i<greeting.length()/2;i++){
+                    int start=i;
+                    int end=n-1-i;
+
+                    char stch=greeting.charAt(start);
+                    char ench=greeting.charAt(end);
+
+                    greeting.setCharAt(start,ench);
+                    greeting.setCharAt(end,stch);
+
+                }
+
+        System.out.println(greeting);
+            }
+        }
+
